054ebf33880d03dda0837afa763df8b4e948a356
Frank T. Bergmann <fbergman@caltech.edu>
2013-03-15 13:51:56 +0100

    typo vs9

--- build_all_vs9.bat
+++ build_all_vs9.bat
@@ -4,11 +4,10 @@ REM Utility script building all VS9 release / debug projects
 REM 
 
 SET THIS_DIR=%~dp0
-SET VC_VARS=c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat
 
 REM set up 64bit environment
 @SET WindowsSdkDir=C:\Program Files\Microsoft SDKs\Windows\v6.0A\
-call @"c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat"  %*
+call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars64.bat"  %*
 title Building x64 debug
 cd /d %THIS_DIR%
 call createX86_vs11_x64_debug 
@@ -23,7 +22,7 @@ move tmp tmp_vs9_x64_release
 
 REM set up 32bit environment
 @SET WindowsSdkDir=C:\Program Files\Microsoft SDKs\Windows\v6.0A\
-call @"c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"  %*
+call "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"  %*
 title Building x86 debug
 cd /d %THIS_DIR%
 call createX86_vs11_x86_debug 
