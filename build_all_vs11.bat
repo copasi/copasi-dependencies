0fb45d48fb143f61fb150ae6b4ff781d68464c93
Frank T. Bergmann <fbergman@caltech.edu>
2013-10-21 10:15:10 +0200

    - more changes, skip building shared libsedml

--- build_all_vs11.bat
+++ build_all_vs11.bat
@@ -6,6 +6,20 @@ REM
 SET THIS_DIR=%~dp0
 SET VC_VARS=c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat
 
+REM set up 32bit environment
+call  "%VC_VARS%" x86
+title Building x86 debug
+cd /d %THIS_DIR%
+call createX86_vs11_x86_debug 
+move bin bin_vs11_x86_debug
+move tmp tmp_vs11_x86_debug
+
+title Building x86 release
+cd /d %THIS_DIR%
+call createX86_vs11_x86_release 
+move bin bin_vs11_x86_release 
+move tmp tmp_vs11_x86_release 
+
 REM set up 64bit environment
 call  "%VC_VARS%" x86_amd64
 title Building x64 debug
@@ -20,17 +34,4 @@ call createX86_vs11_x64_release
 move bin bin_vs11_x64_release 
 move tmp tmp_vs11_x64_release 
 
-REM set up 32bit environment
-call  "%VC_VARS%" x86
-title Building x86 debug
-cd /d %THIS_DIR%
-call createX86_vs11_x86_debug 
-move bin bin_vs11_x86_debug
-move tmp tmp_vs11_x86_debug
-
-title Building x86 release
-cd /d %THIS_DIR%
-call createX86_vs11_x86_release 
-move bin bin_vs11_x86_release 
-move tmp tmp_vs11_x86_release 
 
--- build_all_vs11_static.bat
+++ build_all_vs11_static.bat
@@ -7,6 +7,20 @@ SET THIS_DIR=%~dp0
 SET VC_VARS=c:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat
 SET CMAKE_OVERRIDES=-DWITH_STATIC_RUNTIME=ON 
 
+REM set up 32bit environment
+call  "%VC_VARS%" x86
+title Building x86 debug
+cd /d %THIS_DIR%
+call createX86_vs11_x86_debug 
+move bin bin_vs11_static_x86_debug
+move tmp tmp_vs11_static_x86_debug
+
+title Building x86 release
+cd /d %THIS_DIR%
+call createX86_vs11_x86_release 
+move bin bin_vs11_static_x86_release
+move tmp tmp_vs11_static_x86_release
+
 REM set up 64bit environment
 call  "%VC_VARS%" x86_amd64
 title Building x64 debug
@@ -21,17 +35,4 @@ call createX86_vs11_x64_release
 move bin bin_vs11_static_x64_release
 move tmp tmp_vs11_static_x64_release
 
-REM set up 32bit environment
-call  "%VC_VARS%" x86
-title Building x86 debug
-cd /d %THIS_DIR%
-call createX86_vs11_x86_debug 
-move bin bin_vs11_static_x86_debug
-move tmp tmp_vs11_static_x86_debug
-
-title Building x86 release
-cd /d %THIS_DIR%
-call createX86_vs11_x86_release 
-move bin bin_vs11_static_x86_release
-move tmp tmp_vs11_static_x86_release
 
