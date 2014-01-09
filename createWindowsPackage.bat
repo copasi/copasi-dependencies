a26068f9f421d765007bd0c7c8fee56c016fd685
shoops <shoops@Windows-7.(none)>
2014-01-09 14:14:03 -0500

    Added batch files to build windows release package libraries.

---
+++ createWindowsPackage.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rmdir /S /Q tmp
+
+set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=OFF"
+call createWindows.bat
+move bin bin-MD
+
+rmdir /S /Q tmp
+
+set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=ON"
+call createWindows.bat
+move bin bin-MT 
