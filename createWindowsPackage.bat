fb84bd25f0b1f5519ff7aca45a78b8ab7e460ff8
Stefan Hoops <shoops@vbi.vt.edu>
2016-04-18 13:41:06 -0400

    Fixed install location of libraries and includes.

--- createWindowsPackage.bat
+++ createWindowsPackage.bat
@@ -1,12 +1,14 @@
 @echo off
 Setlocal EnableDelayedExpansion
 rmdir /S /Q tmp
+rmdir /S /Q bin-MD
 
 set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=OFF"
 call createWindows.bat
 move bin bin-MD
 
 rmdir /S /Q tmp
+rmdir /S /Q bin-MT
 
 set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=ON"
 call createWindows.bat
