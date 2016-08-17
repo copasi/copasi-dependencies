48b5b90953616af47cc30ea98690f60758f9d4ce
Frank T. Bergmann <fbergman@caltech.edu>
2016-08-17 13:50:55 +0200

    - updated windows build scripts to include zlib / zipper / combine

--- build_all_vs14.bat
+++ build_all_vs14.bat
@@ -5,34 +5,28 @@ REM
 
 SET THIS_DIR=%~dp0
 SET VC_VARS=%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat
+SET CMAKE_OVERRIDES=
+SET STATIC=
 Setlocal EnableDelayedExpansion
 
 REM set up 32bit environment
 call  "%VC_VARS%" x86
-::title Building x86 debug
-::cd /d %THIS_DIR%
-::call createX86_vs14_x86_debug 
-::move bin bin_vs14_x86_debug
-::move tmp tmp_vs14_x86_debug
+title Building x86 debug
+cd /d %THIS_DIR%
+call createX86_vs14_x86_debug 
 
 title Building x86 release
 cd /d %THIS_DIR%
 call createX86_vs14_x86_release 
-move bin bin_vs14_x86_release 
-move tmp tmp_vs14_x86_release 
 
 REM set up 64bit environment
 call  "%VC_VARS%" x86_amd64
 title Building x64 debug
 cd /d %THIS_DIR%
 call createX86_vs14_x64_debug 
-move bin bin_vs14_x64_debug
-move tmp tmp_vs14_x64_debug
 
 title Building x64 release
 cd /d %THIS_DIR%
 call createX86_vs14_x64_release 
-move bin bin_vs14_x64_release 
-move tmp tmp_vs14_x64_release 
 
 
