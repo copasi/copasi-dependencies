7c6cfb61bcb2cf570450e475259bd198632e4fad
Stefan Hoops <shoops@copasi.org>
2017-04-27 15:42:07 -0400

    Added libnuml and implemented partial build support.

--- createWindows.bat
+++ createWindows.bat
@@ -2,6 +2,12 @@
 SET BASE_DIR=%~dp0
 Setlocal EnableDelayedExpansion
 
+IF "%1"=="" (
+  SET TO_BE_BUILD=expat raptor clapack SBW libSBML libnuml libSEDML MML qwt qwtplot3d 
+) ELSE (
+  SET TO_BE_BUILD=%*
+)
+
 if "%BUILD_TYPE%"=="" SET BUILD_TYPE=Release
 if "%BUILD_TOOL%"=="" SET BUILD_TOOL=nmake
 
@@ -15,84 +21,89 @@ if not exist %BASE_DIR%\bin mkdir %BASE_DIR%\bin
 if not exist %BASE_DIR%\bin\include mkdir %BASE_DIR%\bin\include
 if not exist %BASE_DIR%\bin\lib mkdir %BASE_DIR%\bin\lib
 
-REM Build Clapack
-mkdir %BASE_DIR%\tmp\clapack
-cd /d %BASE_DIR%\tmp\clapack
-%CMAKE% -DBUILD_TESTING=OFF %BASE_DIR%\src\clapack
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+FOR %%A IN (%TO_BE_BUILD%) DO (
+  ECHO Building:  %%A
 
-REM Build CPP Unit
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\cppunit
-cd /d %BASE_DIR%\tmp\cppunit
-%CMAKE% %BASE_DIR%\src\cppunit
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==clapack (
+    REM Build Clapack
+    mkdir %BASE_DIR%\tmp\clapack
+    cd /d %BASE_DIR%\tmp\clapack
+    %CMAKE% -DBUILD_TESTING=OFF %BASE_DIR%\src\clapack
+  )
 
-REM Build Expat
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\expat
-cd /d %BASE_DIR%\tmp\expat
-%CMAKE% -DBUILD_shared=OFF -DBUILD_tests=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF %BASE_DIR%\src\expat
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==cppunit (
+    REM Build CPP Unit
+    mkdir %BASE_DIR%\tmp\cppunit
+    cd /d %BASE_DIR%\tmp\cppunit
+    %CMAKE% %BASE_DIR%\src\cppunit
+  )
 
-REM Build Raptor
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\raptor
-cd /d %BASE_DIR%\tmp\raptor
-%CMAKE% %BASE_DIR%\src\raptor
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==expat (
+    REM Build Expat
+    mkdir %BASE_DIR%\tmp\expat
+    cd /d %BASE_DIR%\tmp\expat
+    %CMAKE% -DBUILD_shared=OFF -DBUILD_tests=OFF -DBUILD_tools=OFF -DBUILD_examples=OFF %BASE_DIR%\src\expat
+  )
 
-REM Build SBW
-mkdir %BASE_DIR%\tmp\SBW
-cd /d %BASE_DIR%\tmp\SBW
-%CMAKE% -DWITH_BUILD_BROKER=OFF -DWITH_BUILD_SHARED=OFF %BASE_DIR%\src\core
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==raptor (
+    REM Build Raptor
+    mkdir %BASE_DIR%\tmp\raptor
+    cd /d %BASE_DIR%\tmp\raptor
+    %CMAKE% %BASE_DIR%\src\raptor
+  )
 
-REM Build libSBML
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\libsbml
-cd /d %BASE_DIR%\tmp\libsbml
-%CMAKE% -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_REQUIREDELEMENTS=OFF -DLIBSBML_USE_LEGACY_MATH=ON -DENABLE_RENDER=ON -DENABLE_COMP=ON -DENABLE_FBC=OFF -DENABLE_SPATIAL=OFF -DENABLE_GROUPS=OFF -DWITH_EXPAT=ON -DWITH_LIBXML=OFF -DLIBSBML_DEPENDENCY_DIR=%BASE_DIR%\bin -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DENABLE_UNIVERSAL=ON %BASE_DIR%/src/libSBML
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==SBW (
+    REM Build SBW
+    mkdir %BASE_DIR%\tmp\SBW
+    cd /d %BASE_DIR%\tmp\SBW
+    %CMAKE% -DWITH_BUILD_BROKER=OFF -DWITH_BUILD_SHARED=OFF %BASE_DIR%\src\core
+  )
 
-REM build libSEDML
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\libSEDML
-cd /d %BASE_DIR%\tmp\libSEDML
-%CMAKE% -DLIBSBML_STATIC=ON -DLIBSEDML_SHARED_VERSION=OFF  -DLIBSEDML_SKIP_SHARED_LIBRARY=ON -DLIBSEDML_DEPENDENCY_DIR=%BASE_DIR%/bin -DEXTRA_LIBS=%BASE_DIR%/lib/expat.lib %BASE_DIR%/src/libSEDML
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==libSBML (
+    REM Build libSBML
+    mkdir %BASE_DIR%\tmp\libsbml
+    cd /d %BASE_DIR%\tmp\libsbml
+    %CMAKE% -DENABLE_COMP=ON -DENABLE_LAYOUT=ON -DENABLE_REQUIREDELEMENTS=OFF -DLIBSBML_USE_LEGACY_MATH=ON -DENABLE_RENDER=ON -DENABLE_COMP=ON -DENABLE_FBC=OFF -DENABLE_SPATIAL=OFF -DENABLE_GROUPS=OFF -DWITH_EXPAT=ON -DWITH_LIBXML=OFF -DLIBSBML_DEPENDENCY_DIR=%BASE_DIR%\bin -DLIBSBML_SKIP_SHARED_LIBRARY=ON -DWITH_BZIP2=OFF -DWITH_ZLIB=OFF -DENABLE_UNIVERSAL=ON %BASE_DIR%/src/libSBML
+  )
 
-REM mml
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\mml
-cd /d %BASE_DIR%\tmp\mml
-%CMAKE% %BASE_DIR%\src\mml
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==libnuml (
+    REM build libnuml
+    mkdir %BASE_DIR%\tmp\libnuml
+    cd /d %BASE_DIR%\tmp\libnuml
+    %CMAKE% -DLIBSBML_STATIC=ON -DLIBNUML_SHARED_VERSION=OFF -DLIBNUML_SKIP_SHARED_LIBRARY=ON -DLIBSBML_INCLUDE_DIR=%BASE_DIR%/bin/include/sbml -DLIBSBML_LIBRARY=%BASE_DIR%/bin/lib/libsbml-static.a -DLIBNUML_DEPENDENCY_DIR=%BASE_DIR%/bin -DEXTRA_LIBS=%BASE_DIR%/bin/lib/expat.lib -DWITH_ZLIB=OFF %BASE_DIR%/src/libnuml
+  )
 
-REM Build QWT
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\qwt
-cd /d %BASE_DIR%\tmp\qwt
-%CMAKE% %BASE_DIR%\src\qwt
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==libSEDML (
+    REM build libSEDML
+    mkdir %BASE_DIR%\tmp\libSEDML
+    cd /d %BASE_DIR%\tmp\libSEDML
+    %CMAKE% -DLIBSBML_STATIC=ON -DLIBSEDML_SHARED_VERSION=OFF  -DLIBSEDML_SKIP_SHARED_LIBRARY=ON -DLIBSEDML_DEPENDENCY_DIR=%BASE_DIR%/bin -DEXTRA_LIBS=%BASE_DIR%/bin/lib/expat.lib %BASE_DIR%/src/libSEDML
+  )
 
-REM Build QWT Plot
-cd /d %BASE_DIR%
-mkdir %BASE_DIR%\tmp\qwtplot3d
-cd /d %BASE_DIR%\tmp\qwtplot3d
-%CMAKE% %BASE_DIR%\src\qwtplot3d-qt4
-%BUILD_TOOL% %BUILD_COMMAND%
-%BUILD_TOOL% %INSTALL_COMMAND%
+  IF %%A==mml (
+    REM mml
+    mkdir %BASE_DIR%\tmp\mml
+    cd /d %BASE_DIR%\tmp\mml
+    %CMAKE% %BASE_DIR%\src\mml
+  )
 
+  IF %%A==qwt (
+    REM Build QWT
+    mkdir %BASE_DIR%\tmp\qwt
+    cd /d %BASE_DIR%\tmp\qwt
+    %CMAKE% %BASE_DIR%\src\qwt
+  )
 
-cd /d %BASE_DIR%
+  IF %%A==qwtplot3d (
+    REM Build QWT Plot
+    mkdir %BASE_DIR%\tmp\qwtplot3d
+    cd /d %BASE_DIR%\tmp\qwtplot3d
+    %CMAKE% %BASE_DIR%\src\qwtplot3d-qt4
+  )
+
+  %BUILD_TOOL% %BUILD_COMMAND%
+  %BUILD_TOOL% %INSTALL_COMMAND%
+
+  cd /d %BASE_DIR%
+)
 
--- createWindowsPackage.bat
+++ createWindowsPackage.bat
@@ -1,15 +1,17 @@
 @echo off
 Setlocal EnableDelayedExpansion
+
 rmdir /S /Q tmp
 rmdir /S /Q bin-MD
 
 set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=OFF"
-call createWindows.bat
+call createWindows.bat expat raptor SBW libSBML libnuml libSEDML MML qwt qwtplot3d 
 move bin bin-MD
 
 rmdir /S /Q tmp
 rmdir /S /Q bin-MT
 
 set CMAKE_OVERRIDES="-DWITH_STATIC_RUNTIME=ON"
-call createWindows.bat
+call createWindows.bat expat raptor SBW libSBML libnuml libSEDML
 move bin bin-MT 
+./
