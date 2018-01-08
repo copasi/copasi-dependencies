9d4392df9eceb0038cc5f76be5a6710066d9bbe7
Stefan Hoops <shoops@copasi.org>
2018-01-08 15:15:18 -0500

    createLinux.sh takes now variables BUILD_DIRand INSTALL_DIR into account.

--- createLinux.sh
+++ createLinux.sh
@@ -20,26 +20,29 @@ CMAKE=${CMAKE:="cmake"}
 MAKE=${MAKE:="gmake"}
 command -v $MAKE >/dev/null 2>&1 || { MAKE=make; }
 
+# Build Directory
+BUILD_DIR=${BUILD_DIR:=${DIRECTORY}/tmp}
+[ -d "${BUILD_DIR}" ] || mkdir -p "${BUILD_DIR}"
+
+# Install Directory
+INSTALL_DIR=${INSTALL_DIR:=${DIRECTORY}/bin}
+[ -d ${INSTALL_DIR} ] || mkdir -p ${INSTALL_DIR}
+[ -d ${INSTALL_DIR}/include ] || mkdir ${INSTALL_DIR}/include
+[ -d ${INSTALL_DIR}/lib ] || mkdir ${INSTALL_DIR}/lib
 
 # echo ${BUILD_TYPE}
 # echo ${CMAKE}
 # echo ${MAKE}
 
-[ -d $DIRECTORY/tmp ] || mkdir $DIRECTORY/tmp
-[ -d $DIRECTORY/bin ] || mkdir $DIRECTORY/bin
-[ -d $DIRECTORY/bin/include ] || mkdir $DIRECTORY/bin/include
-[ -d $DIRECTORY/bin/lib ] || mkdir $DIRECTORY/bin/lib
-
-
-COPASI_COMMON_CMAKE_OPTIONS="${COPASI_COMMON_CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=$DIRECTORY/bin"
-COPASI_COMMON_CMAKE_OPTIONS="${COPASI_COMMON_CMAKE_OPTIONS} -DEXTRA_INCLUDE_DIRS=$DIRECTORY/bin/include"
+COPASI_COMMON_CMAKE_OPTIONS="${COPASI_COMMON_CMAKE_OPTIONS} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
+COPASI_COMMON_CMAKE_OPTIONS="${COPASI_COMMON_CMAKE_OPTIONS} -DEXTRA_INCLUDE_DIRS=${INSTALL_DIR}/include"
 
 COPASI_CMAKE_OPTIONS="${COPASI_CMAKE_OPTIONS} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DSELECT_QT=${SELECT_QT}"
 COPASI_CMAKE_OPTIONS="${COPASI_CMAKE_OPTIONS} ${COPASI_COMMON_CMAKE_OPTIONS}"
 
-COPASI_CXXFLAGS="${CXXFLAGS} ${COPASI_CXXFLAGS} -I$DIRECTORY/bin/include"
-COPASI_CFLAGS="${CFLAGS} ${COPASI_CFLAGS} -I$DIRECTORY/bin/include"
-COPASI_LDFLAGS="${LDFLAGS} ${COPASI_LDFLAGS} -L$DIRECTORY/bin/lib"
+COPASI_CXXFLAGS="${CXXFLAGS} ${COPASI_CXXFLAGS} -I${INSTALL_DIR}/include"
+COPASI_CFLAGS="${CFLAGS} ${COPASI_CFLAGS} -I${INSTALL_DIR}/include"
+COPASI_LDFLAGS="${LDFLAGS} ${COPASI_LDFLAGS} -L${INSTALL_DIR}/lib"
 
 function build () {
 case $1 in
@@ -53,15 +56,15 @@ case $1 in
         --enable-doxygen=no \
         --enable-dot=no \
         --enable-shared=no \
-        --prefix=$DIRECTORY/bin
+        --prefix=${INSTALL_DIR}
     CXXFLAGS="${COPASI_CXXFLAGS}" CFLAGS="${COPASI_CFLAGS}" LDFLAGS="${COPASI_LDFLAGS}" $MAKE -j 4 
     CXXFLAGS="${COPASI_CXXFLAGS}" CFLAGS="${COPASI_CFLAGS}" LDFLAGS="${COPASI_LDFLAGS}" $MAKE install
     ;;
 
   expat)
     # build expat
-    mkdir -p $DIRECTORY/tmp/expat
-    cd $DIRECTORY/tmp/expat
+    mkdir -p ${BUILD_DIR}/expat
+    cd ${BUILD_DIR}/expat
     $CMAKE ${COPASI_COMMON_CMAKE_OPTIONS} \
         -DBUILD_shared=OFF \
         $DIRECTORY/src/expat
@@ -71,8 +74,8 @@ case $1 in
 
   raptor)
     # build raptor
-    mkdir -p $DIRECTORY/tmp/raptor
-    cd $DIRECTORY/tmp/raptor
+    mkdir -p ${BUILD_DIR}/raptor
+    cd ${BUILD_DIR}/raptor
     $CMAKE ${COPASI_COMMON_CMAKE_OPTIONS} \
         -DBUILD_shared=OFF \
         $DIRECTORY/src/raptor
@@ -82,8 +85,8 @@ case $1 in
 
   clapack)
     # Build Clapack
-    mkdir -p $DIRECTORY/tmp/clapack
-    cd $DIRECTORY/tmp/clapack
+    mkdir -p ${BUILD_DIR}/clapack
+    cd ${BUILD_DIR}/clapack
     $CMAKE ${COPASI_COMMON_CMAKE_OPTIONS} \
         -DBUILD_TESTING=OFF \
         $DIRECTORY/src/clapack
@@ -93,8 +96,8 @@ case $1 in
 
   MML)
     #build MML
-    mkdir -p $DIRECTORY/tmp/mml 
-    cd $DIRECTORY/tmp/mml
+    mkdir -p ${BUILD_DIR}/mml 
+    cd ${BUILD_DIR}/mml
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
            $DIRECTORY/src/mml
     $MAKE -j 4
@@ -103,8 +106,8 @@ case $1 in
 
   qwt)
     #build qwt 
-    mkdir $DIRECTORY/tmp/qwt 
-    cd $DIRECTORY/tmp/qwt 
+    mkdir ${BUILD_DIR}/qwt 
+    cd ${BUILD_DIR}/qwt 
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         $DIRECTORY/src/qwt
     make -j 4
@@ -113,8 +116,8 @@ case $1 in
 
   qwt-6)
     #build qwt 
-    mkdir $DIRECTORY/tmp/qwt-6 
-    cd $DIRECTORY/tmp/qwt-6
+    mkdir ${BUILD_DIR}/qwt-6 
+    cd ${BUILD_DIR}/qwt-6
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         $DIRECTORY/src/qwt-6
     make -j 4
@@ -123,8 +126,8 @@ case $1 in
 
   qwtplot3d)
     #build qwtplot3d 
-    mkdir $DIRECTORY/tmp/qwtplot3d-qt4
-    cd $DIRECTORY/tmp/qwtplot3d-qt4 
+    mkdir ${BUILD_DIR}/qwtplot3d-qt4
+    cd ${BUILD_DIR}/qwtplot3d-qt4 
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         $DIRECTORY/src/qwtplot3d-qt4
     make -j 4
@@ -133,8 +136,8 @@ case $1 in
 
   SBW)
     #Build SBW
-    mkdir -p $DIRECTORY/tmp/SBW
-    cd $DIRECTORY/tmp/SBW
+    mkdir -p ${BUILD_DIR}/SBW
+    cd ${BUILD_DIR}/SBW
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         -DWITH_BUILD_BROKER=OFF \
         -DWITH_BUILD_SHARED=OFF \
@@ -145,8 +148,8 @@ case $1 in
 
   libSBML)
     # build libsbml
-    mkdir -p $DIRECTORY/tmp/libsbml
-    cd $DIRECTORY/tmp/libsbml
+    mkdir -p ${BUILD_DIR}/libsbml
+    cd ${BUILD_DIR}/libsbml
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         -DENABLE_LAYOUT=ON \
         -DENABLE_REQUIREDELEMENTS=OFF \
@@ -159,7 +162,7 @@ case $1 in
         -DWITH_LIBXML=OFF \
         -DLIBSBML_USE_LEGACY_MATH=ON \
         -DLIBSBML_USE_LEGACY_MATH=ON \
-        -DLIBSBML_DEPENDENCY_DIR=$DIRECTORY/bin \
+        -DLIBSBML_DEPENDENCY_DIR=${INSTALL_DIR} \
         -DLIBSBML_SKIP_SHARED_LIBRARY=ON \
         -DWITH_BZIP2=OFF \
         -DWITH_ZLIB=OFF \
@@ -170,46 +173,46 @@ case $1 in
 
   libnuml)
     # build libnuml
-    mkdir -p $DIRECTORY/tmp/libnuml
-    cd $DIRECTORY/tmp/libnuml
+    mkdir -p ${BUILD_DIR}/libnuml
+    cd ${BUILD_DIR}/libnuml
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         -DLIBSBML_STATIC=ON \
         -DLIBNUML_SHARED_VERSION=OFF \
         -DLIBNUML_SKIP_SHARED_LIBRARY=ON \
-        -DLIBSBML_INCLUDE_DIR=$DIRECTORY/bin/include \
-        -DLIBSBML_LIBRARY=$DIRECTORY/bin/lib/libsbml-static.a \
-        -DLIBNUML_DEPENDENCY_DIR=$DIRECTORY/bin \
-        -DEXTRA_LIBS=$DIRECTORY/bin/lib/libexpat.a \
+        -DLIBSBML_INCLUDE_DIR=${INSTALL_DIR}/include \
+        -DLIBSBML_LIBRARY=${INSTALL_DIR}/lib/libsbml-static.a \
+        -DLIBNUML_DEPENDENCY_DIR=${INSTALL_DIR} \
+        -DEXTRA_LIBS=${INSTALL_DIR}/lib/libexpat.a \
         -DWITH_ZLIB=OFF \
         $DIRECTORY/src/libnuml
     $MAKE -j 4
     $MAKE install
-    [ -e $DIRECTORY/bin/lib/libnuml*.so ] && rm $DIRECTORY/bin/lib/libnuml*.so
+    [ -e ${INSTALL_DIR}/lib/libnuml*.so ] && rm ${INSTALL_DIR}/lib/libnuml*.so
     ;;
 
   libSEDML)
     # build libSEDML
-    mkdir -p $DIRECTORY/tmp/libSEDML
-    cd $DIRECTORY/tmp/libSEDML
+    mkdir -p ${BUILD_DIR}/libSEDML
+    cd ${BUILD_DIR}/libSEDML
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         -DLIBSBML_STATIC=ON \
         -DLIBSEDML_SHARED_VERSION=OFF \
         -DLIBSEDML_SKIP_SHARED_LIBRARY=ON \
-        -DLIBSBML_INCLUDE_DIR=$DIRECTORY/bin/include \
-        -DLIBSBML_LIBRARY=$DIRECTORY/bin/lib/libsbml-static.a \
-        -DLIBSEDML_DEPENDENCY_DIR=$DIRECTORY/bin \
-        -DEXTRA_LIBS=$DIRECTORY/bin/lib/libexpat.a \
+        -DLIBSBML_INCLUDE_DIR=${INSTALL_DIR}/include \
+        -DLIBSBML_LIBRARY=${INSTALL_DIR}/lib/libsbml-static.a \
+        -DLIBSEDML_DEPENDENCY_DIR=${INSTALL_DIR} \
+        -DEXTRA_LIBS=${INSTALL_DIR}/lib/libexpat.a \
         -DWITH_ZLIB=OFF \
         $DIRECTORY/src/libSEDML
     $MAKE -j 4
     $MAKE install
-    [ -e $DIRECTORY/bin/lib/libsedml*.so ] && rm $DIRECTORY/bin/lib/libsedml*.so
+    [ -e ${INSTALL_DIR}/lib/libsedml*.so ] && rm ${INSTALL_DIR}/lib/libsedml*.so
     ;;
 
   zlib)
     # build zlib
-    mkdir -p $DIRECTORY/tmp/zlib
-    cd $DIRECTORY/tmp/zlib
+    mkdir -p ${BUILD_DIR}/zlib
+    cd ${BUILD_DIR}/zlib
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
         $DIRECTORY/src/zlib
     $MAKE -j 4
@@ -218,11 +221,11 @@ case $1 in
 
   libCombine)
      # build libCombine
-    mkdir -p $DIRECTORY/tmp/libCombine
-    cd $DIRECTORY/tmp/libCombine
+    mkdir -p ${BUILD_DIR}/libCombine
+    cd ${BUILD_DIR}/libCombine
     $CMAKE ${COPASI_CMAKE_OPTIONS} \
-        -DCOMBINE_DEPENDENCY_DIR=$DIRECTORY/bin \
-        -DEXTRA_LIBS=$DIRECTORY/bin/lib/libexpat.a \
+        -DCOMBINE_DEPENDENCY_DIR=${INSTALL_DIR} \
+        -DEXTRA_LIBS=${INSTALL_DIR}/lib/libexpat.a \
         -DLIBCOMBINE_SKIP_SHARED_LIBRARY=ON \
         $DIRECTORY/src/libCombine
     $MAKE -j 4
@@ -235,7 +238,7 @@ for b in ${ToBeBuild}; do
   build $b
 done
 
-cd $DIRECTORY/bin
+cd ${INSTALL_DIR}
 
 case "$(uname -m)" in
   'x86_64')
