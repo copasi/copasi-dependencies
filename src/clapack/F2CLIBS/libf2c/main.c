5c55afbf521228ac08ebbffec0aa7bd434e8031f
Frank T. Bergmann <frank.thomas.bergmann@gmail.com>
2026-03-12 14:00:26 +0100

    - just exclude the actual main function

--- src/clapack/F2CLIBS/libf2c/main.c
+++ src/clapack/F2CLIBS/libf2c/main.c
@@ -103,6 +103,7 @@ char **xargv;
 	}
 #endif
 
+#ifndef NO_F2C_INCLUDE_MAIN
  int
 #ifdef KR_headers
 main(argc, argv) int argc; char **argv;
@@ -143,6 +144,7 @@ exit(0);	/* exit(0) rather than return(0) to bypass Cray bug */
 return 0;	/* For compilers that complain of missing return values; */
 		/* others will complain that this is unreachable code. */
 }
+#endif // NO_F2C_INCLUDE_MAIN
 #ifdef __cplusplus
 }
 #endif
